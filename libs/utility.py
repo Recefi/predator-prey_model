dc7207c42aa5cb3e3db9de90e1e2747e0dad2481
recefi <bataev.iv4n@yandex.ru>
2024-05-17 02:15:34 +0300

    fix ut

--- libs/utility.py
+++ libs/utility.py
@@ -9,31 +9,33 @@ def getCallerName(lvl = 2):
     callerName = os.path.splitext(os.path.basename(callerFullFilename))[0]
     return callerName
 
-def checkDirs(dirName, callerName):
+def chkDir(dirName):
     if not os.path.exists(dirName):
         os.mkdir(dirName)
-    if not os.path.exists(dirName + "/" + callerName):
-        os.mkdir(dirName + "/" + callerName)
 
-def readData(fileName, callerName=None):
+def readData(fileName, callerName="", subDirsName=""):
     if not callerName:
         callerFrame = inspect.stack()[1]
         callerFullFilename = callerFrame.filename
         callerFilename = os.path.basename(callerFullFilename)  # get rid of the directory
         callerName = os.path.splitext(callerFilename)[0]  # split filename and extension
+    if subDirsName:
+        callerName += ("/" + subDirsName)
 
-    checkDirs("csv", callerName)
+    chkDir("csv/" + callerName)
     data = pd.read_csv("csv/" + callerName + "/" + fileName + ".csv", index_col=0)
     return data
 
-def writeData(data, fileName, callerName=None):
+def writeData(data, fileName, callerName="", subDirsName=""):
     if not callerName: callerName = getCallerName()
-    
-    checkDirs("csv", callerName)
+    if subDirsName: callerName += ("/" + subDirsName)
+
+    chkDir("csv/" + callerName)
     data.to_csv("csv/" + callerName + "/" + fileName + ".csv", index=True)
 
-def writeImage(fig, fileName, callerName=None):
+def writeImage(fig, fileName, callerName="", subDirsName=""):
     if not callerName: callerName = getCallerName()
+    if subDirsName: callerName += ("/" + subDirsName)
 
-    checkDirs("images", callerName)
+    chkDir("images", callerName)
     fig.savefig("images/" + callerName + "/" + fileName)
